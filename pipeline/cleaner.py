409089e3563780884d2671ad82ba9fc1a941e8be
Javohir Eshonov <eshonovjavohir92@gmail.com>
2026-05-23 20:40:53 +0500

    Add housing type cleaning functionality to the pipeline
    - Introduced a new function `housing_type_cleaner` in `cleaner.py` to standardize housing type values.
    - Updated `master_file.py` to include the new cleaning function in the data processing workflow.

--- pipeline/cleaner.py
+++ pipeline/cleaner.py
@@ -1,6 +1,5 @@
 import pandas as pd
-
-
+import numpy as np
 
 def drop_dublicate(df_cleaned):
     before = len(df_cleaned)
@@ -24,4 +23,28 @@ def price_cleaner(df_cleaned):
     df_cleaned.loc[df_cleaned['currency']=="UZS", 'price_usd'] = (
         df_cleaned.loc[df_cleaned['currency']=="UZS", 'price_usd'] / exchange_rate
     ).round(1)
+    return df_cleaned
+
+def housing_type_cleaner(df_cleaned):
+    valid_housing_type = {
+    'new building': 'new building',
+    'resale': 'resale',
+    'Новостройка': 'new building',
+    'новостройка': 'new building',
+    'Новостройка.': 'new building',
+    'Вторичка,кирпичный дом.': 'new building'
+    }
+
+    def validate_housing_type(text):
+        if pd.isna(text):
+            return np.nan
+
+        if text in valid_housing_type:
+            return valid_housing_type[text]
+        else:
+            return np.nan
+
+        return text
+
+    df_cleaned['housing_type'] = df_cleaned['housing_type'].apply(validate_housing_type)
     return df_cleaned
--- pipeline/master_file.py
+++ pipeline/master_file.py
@@ -3,7 +3,7 @@
 # ELT turdaki pipelini dasturlashga harakat qilaman
 #---------------------------------------------------------------------------------
 import pandas as pd
-from cleaner import drop_dublicate, price_cleaner
+from cleaner import drop_dublicate, price_cleaner, housing_type_cleaner
 
 def extract():
     df = pd.read_csv('data/Praperad/olx_apartments.csv')
@@ -14,8 +14,13 @@ def extract():
 
 def main():
     df = extract()
+
     df_cleaned=drop_dublicate(df)
+
     df_cleaned = price_cleaner(df_cleaned)
+
+    df_cleaned = housing_type_cleaner(df_cleaned)
+
 
 
 
