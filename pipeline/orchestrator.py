b6a623a764295cdfb36bde3fe14550dd98fff12a
Javohir Eshonov <eshonovjavohir92@gmail.com>
2026-05-24 18:09:29 +0500

    Refactor data cleaning pipeline: - Removed `cleaner.py` and `master_file.py` as they were redundant. - Introduced `orchestrator.py` to manage the ETL process. - Enhanced `transformation.py` with improved data validation functions. - Added room validation logic to filter out listings with 7 or more rooms. - Updated data cleaning logic to include detailed print statements for better tracking.

--- pipeline/orchestrator.py
+++ pipeline/orchestrator.py
@@ -1,9 +1,9 @@
+#---------------------------------------------------------------------------------
+# ETL turdaki pipelini dasturlashga harakat qilaman
+#---------------------------------------------------------------------------------
 
-#---------------------------------------------------------------------------------
-# ELT turdaki pipelini dasturlashga harakat qilaman
-#---------------------------------------------------------------------------------
 import pandas as pd
-from cleaner import drop_dublicate, price_cleaner, housing_type_cleaner
+from transformation import drop_duplicate, price_cleaner, housing_type_cleaner, validate_rooms
 
 def extract():
     df = pd.read_csv('data/Praperad/olx_apartments.csv')
@@ -15,13 +15,13 @@ def extract():
 def main():
     df = extract()
 
-    df_cleaned=drop_dublicate(df)
+    df_cleaned=drop_duplicate(df)
 
     df_cleaned = price_cleaner(df_cleaned)
 
     df_cleaned = housing_type_cleaner(df_cleaned)
 
-
+    df_cleaned = validate_rooms(df_cleaned)
 
 
 if __name__ == "__main__":
--- pipeline/transformation.py
+++ pipeline/transformation.py
@@ -1,21 +1,28 @@
 import pandas as pd
 import numpy as np
 
-def drop_dublicate(df_cleaned):
+def drop_duplicate(df_cleaned):
     before = len(df_cleaned)
 
     df_cleaned = df_cleaned.drop_duplicates()
     df_cleaned = df_cleaned.drop_duplicates(subset=['listing_id'])
 
     after = len(df_cleaned)
+    print(40*"*")
+    print("Drop duplicate jarayoni")
+    print(40*"-")
+    print(f"Jami elonlar soni: {before}")
+    print(f"Duplicate soni: {before - after}")
+    print(f"Duplicatelarsiz: {after}")
 
     return df_cleaned
 
 
 def price_cleaner(df_cleaned):
     # Birinchi bo'lib narxi yo'q qatorlarni olib tashlaymiz
-    before1 = len(df_cleaned['price'])
-    df_cleaned.dropna(subset=['price'], inplace=True)
+    before = len(df_cleaned)
+    df_cleaned = df_cleaned.dropna(subset=['price'])
+    after = len(df_cleaned)
 
     # Endi bir xil valyutaga o'tqazib olamiz
     exchange_rate = 12700
@@ -23,7 +30,15 @@ def price_cleaner(df_cleaned):
     df_cleaned.loc[df_cleaned['currency']=="UZS", 'price_usd'] = (
         df_cleaned.loc[df_cleaned['currency']=="UZS", 'price_usd'] / exchange_rate
     ).round(1)
+
+    print(40*"*")
+    print("Price validation jarayoni")
+    print(40*"-")
+    print(f"Price columndagi bosh maydonlar soni: {before - after}")
+    print("price_usd column yaratildi!")
+
     return df_cleaned
+
 
 def housing_type_cleaner(df_cleaned):
     valid_housing_type = {
@@ -43,8 +58,28 @@ def housing_type_cleaner(df_cleaned):
             return valid_housing_type[text]
         else:
             return np.nan
-
         return text
+    
 
     df_cleaned['housing_type'] = df_cleaned['housing_type'].apply(validate_housing_type)
+    print(40*"*")
+    print("housing_type validation jarayoni")
+    print(40*"-")
+    print("Jarayon pajarildi:")
+    print(df_cleaned['housing_type'].value_counts()) 
+
     return df_cleaned
+
+def validate_rooms(df_cleaned):
+    print(4*"\n")
+    print(40*'*')
+    print("rooms validation jarayoni")
+    print(40*"-")
+
+    print(f"Oldin: \n {df_cleaned['rooms'].value_counts().head(9)}")
+
+    df_cleaned_1 = df_cleaned[df_cleaned['rooms'] < 7].copy()
+
+    print(f"Keyin: \n {df_cleaned_1['rooms'].value_counts()}")
+
+    return df_cleaned_1
